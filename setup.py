9f639a753db663907a8a27dc778a4d6a8c55e1df
Marcel Roth <MarcelRoth100@aol.com>
2024-01-10 10:35:37 +0100

    Revert "modularization"
    This reverts commit 7aa19829180f0d0b94b6803f51b89229b0934ed4.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     description='The #1 motion dataset conversion tool!!',
-    long_description=open('Readme.md').read(),
+    long_description=open('README.md').read(),
     install_requires=requirements,
     python_requires='>=3.6',
 )
